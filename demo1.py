c4df602f0566a9207234a24885e38acd05c56eec
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-17 11:23:27 -0700

    update

--- demo1.py
+++ demo1.py
@@ -1,2 +1,14 @@
 first_name = 'Taylor'
 print(first_name)
+
+last_name = 'Galbraith'
+print(last_name)
+
+age = 27 # int
+bank_account = 42933.23 #float 
+loves_code = True #bool
+
+
+# age= "twenty-seven"
+print(age)
+print(type(age))

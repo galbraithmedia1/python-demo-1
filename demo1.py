1f055cb4ea029d8b0c875fcacc6b5f64c097a700
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-17 12:02:36 -0700

    update

--- demo1.py
+++ demo1.py
@@ -11,4 +11,42 @@ loves_code = True #bool
 
 # age= "twenty-seven"
 print(age)
-print(type(age))
+# print(type(age))
+
+if age >= 18:
+    print('I am an adult')
+
+elif age > 13:
+    print('im a teen')
+
+else:
+    print('im a baby')
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+for number in numbers:
+    print(number)
+
+film_list = ['rED', 'black Magic', 'arri']
+
+for film in film_list:
+    print(film.capitalize())
+
+open_file = open("FinalGrades.csv")
+
+# for row in open_file:
+#     print(row)
+
+
+for row in open_file:
+    row = row.split(',')
+    for value in row:
+        if value.startswith('R'):
+            print(value)
+
+
+
+
+
+open_file.close()
+
